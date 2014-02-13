9e1f11074ae0696aad97f47144e6c6a881acd0d7
Arthur Chen <s810011@gmail.com>
2014-02-13 02:14:47 -0800

    set windows to autosize=true

--- SerialPort/Form1.Designer.cs
+++ SerialPort/Form1.Designer.cs
@@ -167,6 +167,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
             this.ClientSize = new System.Drawing.Size(539, 707);
             this.Controls.Add(this.startBurnButton);
             this.Controls.Add(this.startTestButton);
